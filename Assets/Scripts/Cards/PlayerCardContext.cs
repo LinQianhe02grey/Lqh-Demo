67c55b1a2511cd0ceb2ead5fe035b8df46ea2314
柳阳 李 <lliuyang586@gmail.com>
2026-05-29 00:03:20 +0800

    fix: add missing cross-namespace using directives

--- Assets/Scripts/Cards/PlayerCardContext.cs
+++ Assets/Scripts/Cards/PlayerCardContext.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Cardwin.Combat;
 
 namespace Cardwin.Cards
 {
--- Assets/Scripts/UI/InventoryUI.cs
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Cardwin.Inventory;
 
 namespace Cardwin.UI
 {
